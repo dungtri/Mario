1f413c19cce948bafe6caab78c9998934e826daf
dungtri <dungtri.le.developer@outlook.fr>
2019-08-08 14:03:02 +0200

    Update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -1,14 +1,46 @@
-FROM node
+var http = require("http"),
+  url = require("url"),
+  path = require("path"),
+  fs = require("fs")
+port = process.argv[2] || 8888;
 
-ARG arch=arm
-ENV ARCH=$arch
+http.createServer(function (request, response) {
 
-WORKDIR /usr/src/app
-COPY nodeserver.js .
-COPY Source/ .
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
 
-RUN 
+  var contentTypesByExtension = {
+    '.html': "text/html",
+    '.css': "text/css",
+    '.js': "text/javascript"
+  };
 
-EXPOSE 8888
+  fs.exists(filename, function (exists) {
+    if (!exists) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
 
-CMD ["node","nodeserver.js"]
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function (err, file) {
+      if (err) {
+        response.writeHead(500, { "Content-Type": "text/plain" });
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var headers = {};
+      var contentType = contentTypesByExtension[path.extname(filename)];
+      if (contentType) headers["Content-Type"] = contentType;
+      response.writeHead(200, headers);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
